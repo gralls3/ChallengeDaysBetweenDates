329b682407f9bbe2d544c7a82444d9fd8ddfe52e
gralls3 <32280272+gralls3@users.noreply.github.com>
2018-01-08 15:57:54 -0800

    Initial commit

---
+++ ChallengeDaysBetweenDates/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            DateTime TopCalDate = topCalendar.SelectedDate;
+            DateTime BottomCalDate = bottomCalendar.SelectedDate;
+            TimeSpan daysBetweenDates;
+
+            if (TopCalDate > BottomCalDate)
+            {
+                daysBetweenDates = TopCalDate - BottomCalDate;
+                
+            } else
+            {
+                daysBetweenDates = BottomCalDate - TopCalDate;
+            }
+            resutlLabel.Text = daysBetweenDates.Days.ToString();
+        }
+    }
+}
